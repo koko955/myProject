53568ea6b0cdf36fe49098dcec5424509933edc8
koko955 <83970634+koko955@users.noreply.github.com>
2021-09-13 14:24:25 +0300

    Add files via upload

---
+++ ifElse.js
@@ -0,0 +1,11 @@
+function chisla (input){
+let numA = Number(input[0]);
+let numB = Number(input[1]);
+
+    if (numA > numB){
+        console.log(numA);
+    }else {
+        console.log(numB);
+    }
+}
+chisla (["4","5"]);
