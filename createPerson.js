b1c0d90d8f44888cca2a85ab82d592e658b7c0f2
koko955 <83970634+koko955@users.noreply.github.com>
2021-09-17 12:56:45 +0300

    Add files via upload

---
+++ createPerson.js
@@ -0,0 +1,11 @@
+function person(fname, lname, age) {
+
+    let create = {
+        firstName: fname,
+        lastName: lname,
+        age: age
+    };
+
+ return create;
+}
+console.log (person('Kostadin', 'Atanasov', 26))
